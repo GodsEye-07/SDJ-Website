fc80a183cb83fcb67f851ae060c34a29ac985d5d
Ayush Verma <vermaayush.verma76@gmail.com>
2020-12-02 02:02:48 +0530

    Updates

---
+++ precache-manifest.416e98dfeac7cb74eb97e4a6e6171c1f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "089f16d95ad29dec06b9c1b8b6547b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c98dcfdd75dffa628c15",
+    "url": "/static/css/2.bbefbc3e.chunk.css"
+  },
+  {
+    "revision": "e2014a957a3ebc02fad3",
+    "url": "/static/css/main.14c22966.chunk.css"
+  },
+  {
+    "revision": "c98dcfdd75dffa628c15",
+    "url": "/static/js/2.3adb1abf.chunk.js"
+  },
+  {
+    "revision": "9c4a88d9b57cf919f2c23e04e5eabae2",
+    "url": "/static/js/2.3adb1abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2014a957a3ebc02fad3",
+    "url": "/static/js/main.66f7a5a5.chunk.js"
+  },
+  {
+    "revision": "386bf3ee61be9ec28b68",
+    "url": "/static/js/runtime-main.3a289f92.js"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
